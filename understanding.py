b6222f491f2ae3b5ecf712a0306da1374a36ac2c
WarriorX21 <diptimayee11@gmail.com>
2021-04-27 17:42:05 +0000

    understanding python basics

---
+++ understanding.py
@@ -0,0 +1,47 @@
+x = 10
+y = 19.423
+z = 54j
+num = 10>9
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(num))
+#print should be used individually for checking the type
+
+
+x = "Hello"
+print(type(x))
+print(x[1])
+#index numbers start from 0
+
+
+fruits = ["apple","mango","banana","kiwi"]
+print(fruits)
+print(type(fruits))
+fruits.append ('orange')
+fruits.insert (2, 'tomato')
+print(fruits)
+#while appending or inserting any string value, use siingle inverted commas and space
+
+
+animals = {'reptiles':'snake',
+            'mammals':'whale',
+            'amphibians':'frogs'
+        }
+print(animals)
+print(type(animals))
+animals ['water animals'] = ('fishes')
+print(animals)
+#remember 34th step for adding anything
+
+
+Land_animals = ('lion','tiger','monkey')
+print(type(Land_animals))
+print(Land_animals)
+#don't use space while writing names. Use underscore instead
+
+
+set = {10, 20, 30, 'courses', 30, 40}
+print(set)
+print(type(set))
